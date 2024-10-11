fb280c269bd732eb2c50d6abf688bbb2fe4dedb4
yassine <yassineerraoucha5@gmail.com>
2024-10-11 12:43:11 +0200

    ajouter AfficheArguments.java

---
+++ AfficheArguments.java
@@ -0,0 +1,10 @@
+public class  AfficheArguments{
+    public static void main (String[] arg){
+       System.out.println("nombre d'arguments: "+arg.length);
+       for(int i=0 ; i<arg.length ; i++){
+        System.out.println("argument "+i+" : "+arg[i]);
+        
+       }
+    
+    }
+}
